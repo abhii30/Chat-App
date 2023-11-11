7797c4780ff4c6e8dfd99f2a4a1da37850f54d83
abhii30 <akumar300599@gmail.com>
2023-11-11 15:52:23 +0530

    fetch data

---
+++ frontend/src/Pages/Chat.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const Chat = () => {
+  const fetchChats = async () => {
+    const data = await axios.get("./api/chat");
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return <div>Chat</div>;
+};
+
+export default Chat;
